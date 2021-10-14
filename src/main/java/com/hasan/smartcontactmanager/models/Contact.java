0aad5f3bbcb6afa6c77bd8ea083d5a9e50d367a3
Hasan <hasanuzzaman.dev@gmail.com>
2021-10-14 13:37:03 +0600

    Initial commit

---
+++ src/main/java/com/hasan/smartcontactmanager/models/Contact.java
@@ -0,0 +1,97 @@
+package com.hasan.smartcontactmanager.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CONTACT")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int cId;
+    private String name;
+    private String secondName;
+    private String work;
+    private String phone;
+    private String email;
+    private String imageUrl;
+    @Column(length = 50000)
+    private String description;
+
+    @ManyToOne
+    private User user;
+
+    public Contact() {
+    }
+
+    public int getcId() {
+        return cId;
+    }
+
+    public void setcId(int cId) {
+        this.cId = cId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/test/java/com/hasan/smartcontactmanager/SmartContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan.smartcontactmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

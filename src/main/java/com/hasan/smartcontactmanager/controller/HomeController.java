c4f8b1fd0ba0123c4e482c217f3601b8af994f76
Hasan <hasanuzzaman.dev@gmail.com>
2021-10-14 15:51:39 +0600

    Setting up Bootstrap and Templates | Smart Contact Manager

--- src/main/java/com/hasan/smartcontactmanager/controller/HomeController.java
+++ src/main/java/com/hasan/smartcontactmanager/controller/HomeController.java
@@ -5,13 +5,15 @@ import com.hasan.smartcontactmanager.models.User;
 import com.hasan.smartcontactmanager.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @Autowired
+/*    @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/test")
@@ -26,6 +28,19 @@ public class HomeController {
         userRepository.save(user);
 
         return "Working";
+    }*/
+
+    @RequestMapping("/home")
+    private String home(Model model){
+        model.addAttribute("title", "Home - Smart Contact Manager");
+        return "home";
     }
+
+    @RequestMapping("/about")
+    private String about(Model model){
+        model.addAttribute("title", "About - Smart Contact Manager");
+        return "about";
+    }
+
 
 }

328eebf448ef8a7596d1e69b5e0670bf74e4b246
Hasan <hasanuzzaman.dev@gmail.com>
2021-10-19 11:54:09 +0600

    we are going to learn how to make a login system using spring security. we will learn how to add spring boot starter security dependency and understand all basic concepts of spring security then we will implement a login system with spring security in our project.

---
+++ src/main/java/com/hasan/smartcontactmanager/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.hasan.smartcontactmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/index")
+    public String dashBoard(){
+        return "normal/user_dashboard";
+    }
+}

f940c96d343aab0de5bab82c676f66ee802d3ffa
Hasan <hasanuzzaman.dev@gmail.com>
2021-10-19 15:04:14 +0600

    3 Steps for Role-Based Authorization Spring Security

--- src/main/java/com/hasan/smartcontactmanager/repositories/UserRepository.java
+++ src/main/java/com/hasan/smartcontactmanager/repositories/UserRepository.java
@@ -2,6 +2,11 @@ package com.hasan.smartcontactmanager.repositories;
 
 import com.hasan.smartcontactmanager.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    public User getUserByUserName(@Param("email") String email);
 }

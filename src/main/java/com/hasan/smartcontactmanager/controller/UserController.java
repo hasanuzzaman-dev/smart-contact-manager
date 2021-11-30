8128acb170c02a1e021116091b7c7751787cf4a5
Hasan <hasanuzzaman.dev@gmail.com>
2021-11-30 15:31:02 +0600

    we are going to learn how to work on user dashboard?how to get current logged-in user? Working on User Dashboard.

--- src/main/java/com/hasan/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/hasan/smartcontactmanager/controller/UserController.java
@@ -1,14 +1,31 @@
 package com.hasan.smartcontactmanager.controller;
 
+import com.hasan.smartcontactmanager.models.User;
+import com.hasan.smartcontactmanager.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/index")
-    public String dashBoard(){
+    public String dashBoard(Model model, Principal principal){
+        String userName = principal.getName();
+        System.out.println("USERNAME: "+userName);
+
+        // get the username using username
+        User user = userRepository.getUserByUserName(userName);
+        System.out.println("USER: "+user.toString());
+        model.addAttribute("user", user);
+
         return "normal/user_dashboard";
     }
 }

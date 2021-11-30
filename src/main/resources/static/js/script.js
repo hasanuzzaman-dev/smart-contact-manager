6d554ad5aa9fc348d90f866dcc950b130fa92a0d
Hasan <hasanuzzaman.dev@gmail.com>
2021-11-30 18:05:14 +0600

    create our own responsive Sidebar in very simple steps

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,15 @@
-console.log("This is script file")
+console.log("Hello Base")
+const toggleSidebar = () =>{
+    console.log("toggleSidebar");
+
+    if ($(".sidebar").is(":visible")){
+
+        $(".sidebar").css("display","none");
+        $(".content").css("margin-left","0%");
+
+    }else {
+        $(".sidebar").css("display","block");
+        $(".content").css("margin-left","20%");
+
+    }
+};

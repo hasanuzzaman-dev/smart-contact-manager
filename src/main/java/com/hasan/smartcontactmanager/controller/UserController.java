a8d93a27634245eea28bcc21f72a872778520ece
Hasan <hasanuzzaman.dev@gmail.com>
2021-12-02 15:06:16 +0600

    create controller

--- src/main/java/com/hasan/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/hasan/smartcontactmanager/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.hasan.smartcontactmanager.controller;
 
+import com.hasan.smartcontactmanager.models.Contact;
 import com.hasan.smartcontactmanager.models.User;
 import com.hasan.smartcontactmanager.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -16,16 +19,26 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/index")
-    public String dashBoard(Model model, Principal principal){
+    // This method run for every method index, add_contact or etc.
+    // Method for adding common data for response.
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
         String userName = principal.getName();
-        System.out.println("USERNAME: "+userName);
-
-        // get the username using username
         User user = userRepository.getUserByUserName(userName);
-        System.out.println("USER: "+user.toString());
         model.addAttribute("user", user);
+    }
 
+    @RequestMapping("/index")
+    public String dashBoard(Model model, Principal principal) {
+        model.addAttribute("title", "User Dashboard");
         return "normal/user_dashboard";
+    }
+
+    // Open add form handler
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model) {
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
     }
 }
